c4e27c875cfb4cc37f7c978129b464f404119d7d
Okaarunn <adityawjy666@gmail.com>
2026-04-17 11:30:32 +0700

    fix: security policy

--- resources/views/intern/login.blade.php
+++ resources/views/intern/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests; default-src 'self' https:; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; img-src 'self' https:;">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Internlog' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
